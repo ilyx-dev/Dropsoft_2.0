512f71f7f3ef2561687ea83659a49db2d41ed20a
VС18044 <vlader989@gmail.com>
2024-10-23 21:12:11 +0400

    fix: refactored sync_swap module, added WETH addresses in networks

--- modules/swaps/SyncSwap/sync_swap.py
+++ modules/swaps/SyncSwap/sync_swap.py
@@ -3,7 +3,7 @@ import time
 from models.interfaces.iswap import ISwap
 from models.params.swap_params import SwapParams
 from w3.core import Client
-from .config_sync import NETWORKS, abi, erc20_abi
+from .config_sync import NETWORKS, abi
 
 logger = logging.getLogger(__name__)
 
@@ -16,149 +16,133 @@ class SyncSwap(ISwap):
 
 
     async def swap(self) -> dict:
-
         client = self._client
         my_address = client.wallet.public_key
 
-        try:
-            network_name = self._params.network.name
-            network_data = NETWORKS.get(network_name)
-            if not network_data:
-                logger.error(f"Network {network_name} not found in config")
-                return {'success': False, 'error': 'Network not found in config'}
+        network_name = self._params.network.name
 
-            contract_address = network_data['contract_address']
-            weth_address = network_data['weth_address']
-            use_vault_false_pools = network_data['use_vault_false_pools']
+        network_data = NETWORKS.get(network_name)
+        contract_address = network_data['contract_address']
+        weth_address = self._params.network.get_token_by_symbol("WETH").address
 
-            token_in = self._params.from_token
-            token_in_symbol = token_in.symbol
-            token_out_symbol = self._params.to_token.symbol
 
-            tokens_data = network_data['tokens']
-            from_token_data = tokens_data.get(token_in_symbol)
+        use_vault_false_pools = network_data['use_vault_false_pools']
 
-            token_in_address = from_token_data['address']
-            decimals = from_token_data['decimals']
+        token_in = self._params.from_token
+        token_out = self._params.to_token
 
-            amount_in = self._params.amount
+        token_in_symbol = token_in.symbol
+        token_out_symbol = token_out.symbol
 
-            pool_identifier = f"{token_in_symbol}-{token_out_symbol}"
+        if token_in.address == None:
+            token_in_address = ETH_ADDRESS
+
+        else:
+            token_in_address = client.w3.async_w3.to_checksum_address(token_in.address)
+            erc20_contract = client.contracts.get_erc20_contract(token_in_address)
+            decimals = await erc20_contract.functions.decimals().call()
+
+        amount_in = self._params.amount
+
+        pool_identifier = f"{token_in_symbol}-{token_out_symbol}"
+        pool = network_data['pools'].get(pool_identifier)
+
+
+        if not pool:
+            pool_identifier = f"{token_out_symbol}-{token_in_symbol}"
             pool = network_data['pools'].get(pool_identifier)
             if not pool:
-                pool_identifier = f"{token_out_symbol}-{token_in_symbol}"
-                pool = network_data['pools'].get(pool_identifier)
-                if not pool:
-                    logger.error(f"Pool not found for pair {token_in_symbol}-{token_out_symbol} in network {network_name}")
-                    return {'success': False, 'error': 'Pool not found in config'}
+                error_message = "Pool not found"
+                raise ValueError(error_message)
 
-            amount_out_min = 0
+        amount_out_min = 0
 
-            sync_swap_router_contract = client.contracts.get_contract(
-                contract_address=contract_address,
-                abi=abi
+        sync_swap_router_contract = client.contracts.get_contract(
+            contract_address=contract_address,
+            abi=abi
+        )
+
+        is_eth_in = token_in_address.lower() == ETH_ADDRESS.lower()
+
+        use_vault_flag = True
+        if pool.lower() in [p.lower() for p in use_vault_false_pools]:
+            use_vault_flag = False
+
+        if is_eth_in:
+            transaction_amount = int(amount_in * 1e18)
+            value = transaction_amount
+            token_in_data_address = weth_address
+
+        else:
+            token_contract = self._client.contracts.get_erc20_contract(token_in_address)
+            transaction_amount = int(amount_in * (10 ** decimals))
+            value = 0
+
+            await self.approve_token(
+                client, token_contract, contract_address, transaction_amount
             )
 
-            is_eth_in = token_in_address.lower() == ETH_ADDRESS.lower()
+            token_in_data_address = token_in_address
 
-            use_vault_flag = True
-            if pool.lower() in [p.lower() for p in use_vault_false_pools]:
-                use_vault_flag = False
+        flag = "2" if token_in_address.lower() == ETH_ADDRESS.lower() else "1"
 
-            if is_eth_in:
-                transaction_amount = int(amount_in * 1e18)
-                value = transaction_amount
-                token_in_data_address = weth_address
-            else:
-                token_contract = client.w3.async_w3.eth.contract(address=token_in_address, abi=erc20_abi)
-                transaction_amount = int(amount_in * (10 ** decimals))
-                value = 0
+        paths = [
+            {
+                'steps': [
+                    {
+                        'pool': pool,
+                        'data': f"0x{token_in_data_address[2:].zfill(64)}"
+                                f"{my_address[2:].zfill(64)}"
+                                f"{flag.zfill(64)}",
+                        'callback': "0x0000000000000000000000000000000000000000",
+                        'callbackData': "0x",
+                        'useVault': use_vault_flag
+                    }
+                ],
+                'tokenIn': token_in_address,
+                'amountIn': transaction_amount
+            }
+        ]
 
-                await self.approve_token(
-                    client, token_contract, contract_address, transaction_amount
-                )
+        deadline = int(time.time()) + 60 * 20
 
-                token_in_data_address = token_in_address
+        encoded_data = sync_swap_router_contract.encode_abi(
+            'swap',
+            args=[paths, amount_out_min, deadline]
+        )
 
+        await client.transactions.send(
+            contract_address=contract_address,
+            encoded_data=encoded_data,
+            tx_value=value,
+        )
 
-            flag = "2" if token_in_address.lower() == ETH_ADDRESS.lower() else "1"
+        logger.info(f"Swap successful: {self._params.amount} {self._params.from_token.symbol} to {self._params.to_token.symbol} in {self._params.network.name}")
 
-            paths = [
-                {
-                    'steps': [
-                        {
-                            'pool': pool,
-                            'data': f"0x{token_in_data_address[2:].zfill(64)}"
-                                    f"{my_address[2:].zfill(64)}"
-                                    f"{flag.zfill(64)}",
-                            'callback': "0x0000000000000000000000000000000000000000",
-                            'callbackData': "0x",
-                            'useVault': use_vault_flag
-                        }
-                    ],
-                    'tokenIn': token_in_address,
-                    'amountIn': transaction_amount
-                }
-            ]
-
-            deadline = int(time.time()) + 60 * 20
-
-            encoded_data = sync_swap_router_contract.encode_abi(
-                'swap',
-                args=[paths, amount_out_min, deadline]
-            )
-
-            tx_hash = await client.transactions.send(
-                contract_address=contract_address,
-                encoded_data=encoded_data,
-                tx_value=value,
-            )
-
-            if 'status' in tx_hash and tx_hash['status'] == 1:
-                logger.info(f"Transaction was successful")
-                return {
-                    'success': True,
-                    'network': self._params.network.name,
-                    'from': token_in_symbol,
-                    'to': token_out_symbol,
-                    'amount': amount_in
-                }
-            else:
-                logger.error(f"Transaction failed: 0x{tx_hash}")
-                return {'success': False, 'tx_hash': tx_hash}
-
-        except Exception as e:
-            logger.error(f"Error: {e}")
-            # if 'expected a bool, int, byte or bytearray in first arg, or keyword of hexstr or text' in str(e):
-            #     return {
-            #         'success': True,
-            #         'network': self._params.network.name,
-            #         'from': token_in_symbol,
-            #         'to': token_out_symbol,
-            #         'amount': amount_in
-            #     }
-            # else:
-            #     return {'success': False, 'error': str(e)}
+        return {
+            'network': self._params.network.name,
+            'from': token_in_symbol,
+            'to': token_out_symbol,
+            'amount': amount_in
+        }
 
 
     async def approve_token(self, client, token_contract, spender, amount):
-        try:
-            owner = client.wallet.public_key
-            current_allowance = await token_contract.functions.allowance(owner, spender).call()
-            logger.info(f"Allowance: {current_allowance}")
+        owner = client.wallet.public_key
+        current_allowance = await token_contract.functions.allowance(owner, spender).call()
+        logger.info(f"Allowance: {current_allowance}")
 
-            if current_allowance >= amount:
-                return
+        if current_allowance >= amount:
+            return
 
-            encoded_data = token_contract.encode_abi(
-                'approve',
-                args=[spender, amount]
-            )
+        encoded_data = token_contract.encode_abi(
+            'approve',
+            args=[spender, amount]
+        )
 
-            tx_hash = await client.transactions.send(
-                contract_address=token_contract.address,
-                encoded_data=encoded_data,
-            )
+        await client.transactions.send(
+            contract_address=token_contract.address,
+            encoded_data=encoded_data,
+        )
 
-        except Exception as e:
-            logger.error(f"Approve error: {e}")
+
